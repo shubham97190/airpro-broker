67ae2354a654320201cbb60d115f1494377681f8
Shubham Prajapati <shubhamprajapati.bca2014@gmail.com>
2024-04-15 11:23:22 +0530

    intgrate api

--- read/mqtt/dto/client_dto.py
+++ read/mqtt/dto/client_dto.py
@@ -1,3 +1,4 @@
+from typing import Union
 from dataclasses import dataclass
 
 
@@ -11,3 +12,11 @@ class SubsDto:
     passwd: str = None
     b_port: str = None
     broker_ip: str = None
+
+@dataclass(frozen=True)
+class StatisticsDto:
+    serial_num: str
+    device_id: str
+    mac_addr: str
+    data: Union[dict, list]
+    
--- read/mqtt/parser/statistics_parser.py
+++ read/mqtt/parser/statistics_parser.py
@@ -1,8 +1,9 @@
 import logging
 
 from protobuf.aircnms_stats_pb2 import Report
+from read.mqtt.dto.client_dto import StatisticsDto
 from read.mqtt.parser.interface import ParserInterface
-
+from db.client import Client
 logger = logging.getLogger(__name__)
 
 
@@ -15,13 +16,26 @@ class StatisticsParser(ParserInterface):
         logger.info("Statistics Parser initiated for payload %s", payload)
         message_to_dict = super(StatisticsParser, self).extract_data(payload)
         if message_to_dict:
+            client = Client()
             logger.info("Statistics serialNum  %s", message_to_dict.serial_num)
             logger.info("Statistics device_id  %s", message_to_dict.device_id)
             logger.info("Statistics mac_addr %s", message_to_dict.mac_addr)
-            logger.info("Statistics clients data  %s", message_to_dict.clients)
-            logger.info("Statistics device data  %s", message_to_dict.device)
-            logger.info("Statistics vif data  %s", message_to_dict.vif)
-            logger.info("Statistics neighbors data  %s", message_to_dict.neighbors)
-
+            client_data = StatisticsDto(message_to_dict.serial_num, message_to_dict.device_id, message_to_dict.mac_addr, message_to_dict.clients)
+            logger.info("Statistics clients data  %s", client_data)
+            client.connected_client.create_client(client_data)
+            
+            device_data = StatisticsDto(message_to_dict.serial_num, message_to_dict.device_id, message_to_dict.mac_addr, message_to_dict.device)
+            logger.info("Statistics device data  %s", device_data)
+            client.device.create_device(device_data)
+            
+            vif_data = StatisticsDto(message_to_dict.serial_num, message_to_dict.device_id, message_to_dict.mac_addr, message_to_dict.vif)
+            logger.info("Statistics vif data  %s", vif_data)
+            client.vif.create_vif(vif_data)
+            
+            neighbors_data = StatisticsDto(message_to_dict.serial_num, message_to_dict.device_id, message_to_dict.mac_addr, message_to_dict.neighbors)
+            logger.info("Statistics neighbors data  %s", message_to_dict.neighbors_data)
+            client.neighbor.create_neighbor(neighbors_data)
+            
+            
     def serialize(self):
         pass
